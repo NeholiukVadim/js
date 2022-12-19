651a62ba17558f342072c755c4ba30c120e5aba4
vadim <vadim.negoluk@gmail.com>
2022-12-19 18:28:01 +0200

    task-2.3 make a sort button

---
+++ task3/script.js
@@ -0,0 +1,147 @@
+const menu = document.querySelector(".menu-item-conteiner");
+const filterButtonAll = document.querySelector(".all-button");
+const filterButtonSalads = document.querySelector(".salad-button");
+const filterButtonDinners = document.querySelector(".dinner-button");
+const filterButtonDrinks = document.querySelector(".drinks-button");
+const filterButtonDeserts = document.querySelector(".deserts-button");
+const sortButton = document.querySelector(".sort-button");
+
+const menuItems = [{
+   photo: "images/pancakes.jpg",
+   title: "Pancake",
+   price: "$9.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["desert", "dinner"]
+},
+{
+   photo: "images/soup.jpg",
+   title: "Soup",
+   price: "$4.50",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["dinner"]
+},
+{
+   photo: "images/pizza.jpg",
+   title: "Pizza",
+   price: "$11.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["dinner"]
+},
+{
+   photo: "images/cake.jpg",
+   title: "Cake",
+   price: "$13.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["desert"]
+},
+{
+   photo: "images/juice.jpg",
+   title: "Juice",
+   price: "$3.50",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["drink", "desert"]
+},
+{
+   photo: "images/salad.jpg",
+   title: "Salad",
+   price: "$6.50",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["salad"]
+},
+{
+   photo: "images/burger.jpg",
+   title: "Burger",
+   price: "$8.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["dinner"]
+},
+{
+   photo: "images/milkshake.jpg",
+   title: "Milkshake",
+   price: "$3.50",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["desert", "drink"]
+},
+{
+   photo: "images/cupcake.jpg",
+   title: "Cupcake",
+   price: "$7.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["desert"]
+},
+{
+   photo: "images/steak.jpg",
+   title: "Steak",
+   price: "$11.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["dinner"]
+},
+{
+   photo: "images/pasta.jpg",
+   title: "Pasta",
+   price: "$9.00",
+   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quaerat rerum suscipit possimus enim at reprehenderit velit exercitationem unde.",
+   catagory: ["dinner"]
+},
+];
+
+const getMenuItem = (menuItem) => {
+   return ` 
+   <div class="menu-item">
+      <img src="${menuItem.photo}" class="menu-item-img">
+      <div class="menu-item-text">
+         <p class="menu-item-article">${menuItem.title}<span class="price"> ${menuItem.price}</span></p>
+         <div class="menu-item-description">${menuItem.description}</div>
+      </div>
+   </div>
+`
+};
+
+let currentMenu = menuItems;
+
+function filterMenu(foodType) {
+   return menuItems.filter(menuItem => menuItem.catagory.includes(foodType))
+};
+
+function sortMenu(currentMenu) {
+   return currentMenu.sort((a, b) => {
+      if (a.title.length === b.title.length) {
+         return a.title.localeCompare(b.title);
+      } else {
+         return a.title.length - b.title.length;
+      }
+   });
+};
+
+function showMenu(menuItems) {
+   menu.innerHTML = ""
+   menuItems.forEach(function (menuItem) {
+      menu.innerHTML += getMenuItem(menuItem)
+   })
+};
+
+filterButtonAll.addEventListener("click", () => showMenu(menuItems));
+
+filterButtonSalads.addEventListener("click", () => {
+   currentMenu = filterMenu("salad");
+   showMenu(currentMenu);
+});
+
+filterButtonDinners.addEventListener("click", () => {
+   currentMenu = filterMenu("dinner");
+   showMenu(currentMenu);
+});
+
+filterButtonDrinks.addEventListener("click", () => {
+   currentMenu = filterMenu("drink");
+   showMenu(currentMenu);
+});
+
+filterButtonDeserts.addEventListener("click", () => {
+   currentMenu = filterMenu("desert");
+   showMenu(currentMenu);
+});
+
+sortButton.addEventListener("click", () => showMenu(sortMenu(currentMenu)));
+
+showMenu(menuItems);
